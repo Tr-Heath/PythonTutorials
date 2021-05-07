280a4d6e1b99c3b2588b7e61bdf97518e6af7d1d
Travis Heath <travis.heath@b3groupinc.com>
2021-05-06 17:28:30 -0700

    Little string exercise to start off

---
+++ Day1/main.py
@@ -0,0 +1,9 @@
+#Simple string manipulation exercise because as I've discovered, my brain is determined to forget everything I have learned.
+#And now that I sit in meetings all day my programming knowledge is rapidly deteriorating 
+
+print("Band name generator or data scraper, you decide!")
+town = input("From which town do you hail? ")
+pet = input("What is your favorite pet's name? ")
+petNameLength = len(pet)
+print("Search no farther, you will now be known as " + town + " " + pet + " and the "+ str(petNameLength) + ".")
+
