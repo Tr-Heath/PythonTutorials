96e1a659ec1d300c8f62b5a70136fb598c114820
Travis Heath <travis.heath@outlook.com>
2021-05-07 16:22:50 -0700

    add formatting

--- Day2/tip_calc.py
+++ Day2/tip_calc.py
@@ -8,5 +8,7 @@ total = float(input("What was the total bill? "))
 tip = 1 + (int(input("What should the tip percentage be? ")) / 100)
 numPeople = int(input("How many people are paying for the bill? "))
 perPerson = (total / numPeople) * tip
+formatPerPerson = "{:.2f}".format(perPerson,2)
 
-print(f"The amount per person should be: {round(perPerson,2)}")
+
+print(f"The amount per person should be: ${formatPerPerson}")
