494c6f673ac31d4f3c421d675da181751240b8aa
Travis Heath <travis.heath@outlook.com>
2021-05-07 16:16:26 -0700

    Final tutorial project for the day.

---
+++ Day2/tip_calc.py
@@ -0,0 +1,12 @@
+#build a tip calc
+#Ask for the total bill, percentage to give and the number of ways to split the bill.
+#Each person should pay round((bill / numPeople) * tipPercentage num,places)
+
+#TODO: tip calculation should be a function, validate input.
+print("Welcome to the tip calculator.")
+total = float(input("What was the total bill? "))
+tip = 1 + (int(input("What should the tip percentage be? ")) / 100)
+numPeople = int(input("How many people are paying for the bill? "))
+perPerson = (total / numPeople) * tip
+
+print(f"The amount per person should be: {round(perPerson,2)}")
