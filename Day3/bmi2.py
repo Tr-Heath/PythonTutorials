ac32c70d9d0215c8882859b002fca6e0a089b726
Travis Heath <travis.heath@outlook.com>
2021-05-08 17:16:55 -0700

    Update previous BMI calc as another conditional exercise

---
+++ Day3/bmi2.py
@@ -0,0 +1,22 @@
+#Enhance the BMI calculator from Day 2.
+#Give an interperatation of the value given.
+
+def categorizeBMIValue(bmi):
+    if bmi < 18.5:
+        print(f"A BMI of {bmi} is underweight.")
+    elif bmi >= 18.5 and bmi < 25:
+        print(f"A BMI of {bmi} is a normal weight.")
+    elif bmi >= 25 and bmi < 30:
+        print(f"A BMI of {bmi} is a overweight.")
+    elif bmi >= 30 and bmi < 35:
+        print(f"A BMI of {bmi} is a obese.")
+    else:
+        print(f"A BMI of {bmi} is a clinically obese.")
+
+def calcBMI(height, weight):
+    return round(weight / height ** 2, 1)
+
+height = float(input("Enter your height in meters: "))
+weight = float(input("Enter your weight in kg: "))
+
+categorizeBMIValue(calcBMI(height, weight))
