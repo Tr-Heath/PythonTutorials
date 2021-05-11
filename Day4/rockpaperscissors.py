d1d34d1a8be6732333ff0707f24112028a749f62
Travis Heath <travis.heath@outlook.com>
2021-05-11 16:01:07 -0700

    Add in game win condition and improve image display code

--- Day4/rockpaperscissors.py
+++ Day4/rockpaperscissors.py
@@ -2,20 +2,30 @@
 import random, gameimages
 
 #pass an int 0-2 and return image of choice
-def choiceImage(comp):
-    if int(comp) == 0:
-        return gameimages.rock
-    elif int(comp) == 1:
-        return gameimages.paper
-    else:
-        return gameimages.scissors
+# def choiceImage(image):
+#     if int(image) == 0:
+#         return gameimages.rock
+#     elif int(image) == 1:
+#         return gameimages.paper
+#     else:
+#         return gameimages.scissors
+
+def winCondition(computer, player):
+    if(computer == player):
+        print("Tie! Try again!")
+    elif(gameimages.game_images[computer] == gameimages.game_images[player-1]):
+        print("You win!")
+    else: 
+        print("You lose!")
 
 print("Welcome to the game rock, paper scissors!")
-choice = input("Please type your choice. '0' for rock, '1' for paper and '2' for scissors.")
-compImage = choiceImage(random.randint(0, 2))
-playerImage = choiceImage(choice)
+player = int(input("Please type your choice. '0' for rock, '1' for paper and '2' for scissors."))
+computerChoice = random.randint(0,2)
+#compImage = choiceImage(random.randint(0, 2))
+#playerImage = choiceImage(choice)
 
 print(f"You have chosen:")
-print(playerImage)
+print(gameimages.game_images[player])
 print("Computer has:")
-print(compImage)
+print(gameimages.game_images[computerChoice])
+winCondition(computerChoice, player)
