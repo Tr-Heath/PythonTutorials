773a512dabd29e3ff2d2d1f134e4df18452593f2
Travis Heath <travis.heath@outlook.com>
2021-05-17 21:49:47 -0700

    In progress changing game logic to class structure. Not functional quite yet, but a stopping point

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -5,49 +5,66 @@
 #Array to keep guesses so far, initialize to array of '_' to the length of the word.
 import random
 
-#Step 1 
-
+MAX_GUESSES = 6
 word_list = ["aardvark", "baboon", "camel"]
-game_word = ""
 
+class Player_State:
+    word = []
+    game_word = ""
+    guess_count = 0
+    recent_guess = ""
+    game_lost = False
+    
+    def __init__(self, game_word):
+        for letter in game_word:
+            self.word.append("_")
+
+    def get_user_choice(self):
+        self.recent_guess = input("Guess a letter: ").lower()
+    
+    #the correct_list list tracks the indexes in which the guess was found to update the state of player word
+    def check_guess(self):
+        index = 0
+        correct_list = []
+        guess_found = False
+        for letter in game_word:
+            if self.recent_guess == letter:
+                guess_found = True
+                correct_list.append(index)
+            index += 1
+        print(self.word)
+        if guess_found:
+            return self.set_player_word(correct_list)
+        else:
+            self.guess_count += 1
+            return self.word
+
+    def set_player_word(self, correct_list):
+        print("set player word")
+        for num in correct_list:
+            player.word[num] = player.recent_guess
 
 def init_game(word_list):
     print("Welcome to the game of hangman!")
     print("Can you complete the word before time runs out?")
     return random.choice(word_list)
 
-def init_player(game_word):
-    player_word = []
-    for letter in game_word:
-        player_word.append("_")
-    return player_word
-
-def get_user_choice():
-    guess = input("Guess a letter: ").lower()
-    return guess
-
-#the correct_list list tracks the indexes in which the guess was found to update the state of player word
-def check_guess(guess, player_word, game_Word):
-    index = 0
-    correct_list = []
-    guess_found = False
-    for letter in game_word:
-        if guess == letter:
-            guess_found = True
-            correct_list.append(index)
-        index += 1
-    print(correct_list)
-    if guess_found:
-        set_player_word(player_word, correct_list, guess)
-
-def set_player_word(player_word, correct_list, guess):
-    for num in correct_list:
-        player_word[num] = guess
-    return player_word
+def end_game(loss):
+    if loss:
+        print(f"Game over! You lost, the answer was {game_word}")
+    else:
+        print(f"Game over! You win!")
 
 game_word = init_game(word_list)
-player_word = init_player(game_word)
-print(player_word)
-guess = get_user_choice()
-check_guess(guess, player_word, game_word)
-print(player_word)
+player = Player_State(game_word)
+print(player)
+print(game_word)
+while "_" in player.word:
+    print(f"You have used {player.guess_count} of 6 guesses")
+    if player.guess_count == MAX_GUESSES:
+        player.game_lost = True
+        break
+    player.get_user_choice()
+    player.check_guess()
+
+end_game(player.game_lost)
