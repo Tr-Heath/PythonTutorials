6af08ab245c2250f7acddca523ea59f1f03ec78b
Travis Heath <travis.heath@outlook.com>
2021-05-20 08:23:14 -0700

    remove need to import math

--- Day8/wallpaintcalc.py
+++ Day8/wallpaintcalc.py
@@ -1,13 +1,12 @@
 #warm up exercise
 #Calculate how much paint is needed to cover a wall of a given height width
-import math
 
 COVERAGE = 375
 height = 0
 width = 0
 
 def getPaintAmount(height, width):
-    return math.ceil((height*width) / COVERAGE)
+    return int((height*width) / COVERAGE) + ((height*width) % COVERAGE > 0)
 
 width = float(input("What is the width of the wall in feet? "))
 height = float(input("What is the height of the wall in feet? "))
