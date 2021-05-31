e034c5b6e25c0dede389ffbb1b4b6088b19fb4f8
Travis Heath <travis.heath@outlook.com>
2021-05-30 19:22:44 -0700

    tutorial work

---
+++ Day9/auction.py
@@ -0,0 +1,17 @@
+#Day 9, working with dictionaries in Python, exercised with simple auction tracking app
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+order = {
+    "starter": {1: "Salad", 2: "Soup"},
+    "main": {1: ["Burger", "Fries"], 2: ["Steak"]},
+    "dessert": {1: ["Ice Cream"], 2: []},
+}
+
+print(order["main"][2][0])
